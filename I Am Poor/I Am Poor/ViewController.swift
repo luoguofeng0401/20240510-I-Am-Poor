2b1de25b85a065284b366a8fc87ebf0dab44fd0e
luoguofeng0401 <56261249+luoguofeng0401@users.noreply.github.com>
2024-05-10 22:16:43 +0800

    Add files via upload

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Guo-Feng Luo on 2024/5/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
